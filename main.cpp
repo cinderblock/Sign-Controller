33c4f2bd2dc7d72a6f8e52138697fbf71ac1c924
Cameron Tacklind <cameron@tacklind.com>
2017-08-24 02:21:34 -0700

    New animation. Still broken...

--- main.cpp
+++ main.cpp
@@ -14,69 +14,69 @@
 
 #include <math.h>
 
-unsigned int delay = 50;
+unsigned int delay = 500;
 
+void digit(u1 i) {
+    Display::writePixel(i, 255);
+
+    Display::latch();
+    _delay_ms(delay);
+    Display::writePixel(i, 0);
+    Display::latch();
+}
 
 void main() {
- 
- Board::A.off();
- Board::B.off();
- Board::C.off();
- 
- Board::A.output();
- Board::B.output();
- Board::C.output();
-  Board::C.on();
- 
- SoftwarePWM::init();
-  
- 
-  u2 j = 0;
-  while (1) {
-  _delay_ms(10);
-   for (u1 i = 0; i < Display::numPixels; i++) {
-    Display::writePixel(i, (sin(i / 3. - j / 10.) + 1) / 2 * 255);
-   }
 
-   Display::latch();
-   
-   j++;
-  }
- 
- 
- while (1); {
-  _delay_ms(1000);
-  Board::C.on();
-  
-  PORTB = 0b00111110;
-  _delay_ms(delay);
-  PORTB = 0b00111101;
-  _delay_ms(delay);
-  PORTB = 0b00111011;
-  _delay_ms(delay);
-  PORTB = 0b00110111;
-  _delay_ms(delay);
-  PORTB = 0b00101111;
-  _delay_ms(delay);
-  PORTB = 0b00011111;
-  _delay_ms(delay);
-  PORTB = 0b00111111;
-  PORTD = 0b01111110;
-  _delay_ms(delay);
-  PORTD = 0b10111110;
-  _delay_ms(delay);
-  PORTD = 0b11011110;
-  _delay_ms(delay);
-  PORTD = 0b11101110;
-  _delay_ms(delay);
-  PORTD = 0b11110110;
-  _delay_ms(delay);
-  PORTD = 0b11111010;
-  _delay_ms(delay);
-  PORTD = 0b11111100;
-  _delay_ms(delay);
-  PORTD = 0b11111110;
-  
+  Board::A.off();
+  Board::B.off();
   Board::C.off();
- }
+
+  Board::A.output();
+  Board::B.output();
+  Board::C.output();
+  Board::C.on();
+
+  Board::Button.on();
+
+  SoftwarePWM::init();
+
+
+  u2 j = 0;
+  while (0) {
+    _delay_ms(10);
+    for (u1 i = 0; i < Display::numPixels; i++) {
+      Display::writePixel(i, (sin(i / 3. - j / 10.) + 1) / 2 * 255);
+    }
+
+    Display::latch();
+
+    j++;
+  }
+
+
+  while (1) {
+    Board::Double = Board::Button.isHigh();
+
+    _delay_ms(1000);
+    Board::C.on();
+
+    digit( 0);
+    digit( 1);
+    digit( 2);
+    digit( 3);
+    digit( 4);
+    digit( 5);
+    digit( 6);
+    digit( 7);
+    digit( 8);
+    digit( 9);
+    digit(10);
+    digit(11);
+    digit(12);
+    digit(13);
+    digit(14);
+    digit(15);
+
+    Board::C.off();
+  }
 }
